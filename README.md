cc49dc9e3cfc3c07f88638c8dd96f95bc7ef5c7d
jterrenb <76902186+jterrenb@users.noreply.github.com>
2024-09-07 11:20:16 +0200

    FEAt Basic functions add, subtract, multiply, divide, and more
    Basic logic to change from one variable to the other when the operator is clicked.

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ PSEUDOCODE:
 - Do the CSS to make the calculator look good enought.
 - Create the event listener for the action when a click is done in the container who has the calculator.
 - Switch the action depending of the button clicked,storing the value clicked and showing it in the display.
+    - Por defecto el valor de los números pulsados de incluirá en la variable firstNum, la cual no está inicializada.
+    - Establecemos un máximo de 17 caracteres para que sean mostrados en la pantalla y así no hacer overflow.
+    - Cuando se pulsa un número, si la variable carece de valor, se le asignará el valor del contenido del botón. Si tiene ya un valor y no es mayor de 17, se añadirá el valor del contenido del botón a la variable. Si tiene 17 caracteres, entonces no asignará ningún valor adicional.
+    - Una vez pulsado un botón operador, los números que se pulsen deben ser almacenados en la variable secondNum.
+    - Al pulsar el botón de igual o de cualquier operador, debe realizarse la operación matemática, mostrando el resultado en el display.
+    - Si 
--- javascript.js
+++ javascript.js
@@ -1,56 +1,50 @@
-let firstNum = 0;
-let secondNum = 0;
+let nums = [];
+let secondNum;
 let operator;
+let idx = 0;
 const container = document.querySelector(".container");
 let display = document.querySelector(".display");
 function add ( firstNum, secondNum) {
-
+    return +firstNum + +secondNum;
 };
 function subtract (firstNum, secondNum) {
-
+    return +firstNum - +secondNum;
 };
 function multiply (num, multiplier){
-
+    return +num * +multiplier;
 };
 function divide (num, divisor) {
-
+    return +num / +divisor;
 };
 function operate (firstNum, secondNum, operator) {
-    
 };
 
 container.addEventListener("click", (event) => {
     let target = event.target;
     switch (target.className) {
-        case "zero":
-            display.textContent += "0";
+        case "number":
+            if ( nums[idx] === undefined) {
+                nums[idx] = target.textContent;
+            } else {
+                nums[idx].length < 17 ? nums[idx] += target.textContent : nums[idx];
+            }
+            display.textContent = nums[idx];
             break;
-        case "one":
-                display.textContent += "1";
+        case "operator":
+            if (operator === undefined) {
+                    operator = target.textContent;
+                    idx = 1;
+            } else {
+                switch (operator) {
+                    case "+":
+                        display.textContent = add(nums[0], nums[1]);
+                        break;
+                }
+            };
             break;
-        case "two":
-            display.textContent += "2";
+        case "clear":
+            nums.fill("");
+            display.textContent = "";
             break;
-        case "three":
-            display.textContent += "3";
-            break;
-        case "four":
-            display.textContent += "4";
-            break;
-        case "five":
-            display.textContent += "5";
-            break;
-        case "six":
-            display.textContent += "6";
-            break;
-        case "seven":
-            display.textContent += "7";
-            break;
-        case "eight":
-            display.textContent += "8";
-            break;
-        case "nine":
-            display.textContent += "9";
-            break;
-    }
+    };
 });
