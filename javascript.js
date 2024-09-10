4079ef2f02ed3334dada3bbd3234620f6f4605ae
jterrenb <76902186+jterrenb@users.noreply.github.com>
2024-09-10 08:43:23 +0200

    FEAT Keyboard Suported!

--- javascript.js
+++ javascript.js
@@ -102,3 +102,76 @@ container.addEventListener("click", (event) => {
             break;
     };
 });
+
+document.addEventListener("keydown", (event) => {
+    let keyValue = event.key;
+    console.log(keyValue);
+    switch (keyValue) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            if ( nums[idx] === undefined) {
+                nums[idx] = keyValue;
+            } else {
+                nums[idx].length < 15 ? nums[idx] += keyValue : nums[idx];
+            }
+            display.textContent = nums[idx];
+            break;
+        case "/":
+        case "*":
+        case "+":
+        case "-":
+            // target.style.backgroundColor = "lightblue";
+            if (nums[0] === "" && display.textContent !== "") {
+                nums[0] = display.textContent;
+                operator = keyValue;
+                idx = 1;
+            } else if (nums[idx] === undefined) {
+                operator = "";
+                idx = 0;
+                clearBground();
+            } else if (operator === undefined || operator === "") {
+                operator = keyValue;
+                idx = 1;
+            } else {
+                operate(nums[0], nums[1], operator);
+                nums.fill("");
+                clearBground();
+                operator = keyValue;
+                // target.style.backgroundColor = "lightblue";
+                nums[0] = display.textContent;
+                idx = 1;
+            };
+            break;
+        case "clear":
+            nums.fill("");
+            operator = "";
+            display.textContent = "";
+            idx = 0;
+            clearBground();
+            break;
+        case "equal":
+            if ( operator !== undefined) {
+                operate(nums[0], nums[1], operator);
+                operator = "";
+                idx = 0;
+                nums.fill("");
+                clearBground();
+            }
+            break;
+        case ".":
+            stringNum = nums[idx].toString();
+            if (stringNum.includes(".") == false && nums[idx].length > 0) {
+                nums[idx] += keyValue;
+                display.textContent = nums[idx];
+            };
+            break;
+    };
+});
